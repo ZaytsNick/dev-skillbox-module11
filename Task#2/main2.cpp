f5ddccf7a67903f7380e23f22415e961ad874cb5
ZaytsHome <10kcin01@gmail.com>
2023-08-13 11:43:02 +0500

    first commit

---
+++ Task#2/main2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+bool corrected_email(bool have_at, string email)
+{
+    if(have_at)
+    {
+        
+    }
+    else
+    {
+
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello asshole World!\n";
+    return 0;
+}
