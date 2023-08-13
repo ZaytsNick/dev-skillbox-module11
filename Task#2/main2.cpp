02b215a49357a017719aefc60946468e7e1f1185
ZaytsHome <10kcin01@gmail.com>
2023-08-13 20:35:17 +0500

    function#1 Task#2

--- Task#2/main2.cpp
+++ Task#2/main2.cpp
@@ -1,13 +1,52 @@
 #include <iostream>
 using namespace std;
-bool corrected_email(bool have_at, string email)
+bool corrected_first_symbol()
 {
-    if(have_at)
+}
+int corrected_part(int have_at, string part)
+{
+    if ((bool)have_at)
+    string correctedSymbol = "!#$%&'*+-/=?^_`{|}~";
+    for (int i = 0; i < 64; i++)
     {
-        
-    }
-    else
-    {
+        if (part[i] == '.' && i - 1 == -1 ? true : part[i - 1] == '.')
+        {
+            return false;
+        }
+        else if (part[i] >= 'a' && part[i] <= 'z' || part[i] >= 'A' && part[i] <= 'Z')
+        {
+            break;
+        }
+        else if (!(bool)have_at)
+        {
 
+            if (part[i] == '@')
+            {
+
+                i>0?({return true;}):({return false;});
+            }
+        }
+        else
+        {
+            if (part[i] == '@')
+            {
+                return false;
+            }
+            else
+            {
+                for (int j = 0; j < correctedSymbol.length(); j++)
+                {
+                    if (part[i] == correctedSymbol[j])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
     }
+    return false;
 }
