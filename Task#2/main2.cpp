d595bdec1184767f9b2fe2f5dbae10849f3ae1c7
Zayts Work <zaytsev@un74.ru>
2023-08-14 16:11:55 +0500

    Maybe good work

--- Task#2/main2.cpp
+++ Task#2/main2.cpp
@@ -1,44 +1,53 @@
 #include <iostream>
 using namespace std;
-bool corrected_first_symbol()
-{
-}
+
 int corrected_part(int have_at, string part)
 {
-
-    for (int i = have_at; i < ((bool)have_at ? have_at + 63 : 64); i++)
+    if (part[part.length() - 1] == '.')
     {
-        if (part[i] == '.' && i < 1 ? true : part[i - 1] == '.') // checking for the first '.' and for two '.' in a row.
+        return false;
+    }
+    else
+    {
+        int s = (bool)have_at ? have_at + 63 : 64;
+        for (int i = have_at; i < s; i++)
         {
-            return false;
-        }
-        else if (part[i] >= 'a' && part[i] <= 'z' || part[i] >= 'A' && part[i] <= 'Z') // checking for a character from the alphabet.
-        {
-            // break;
-        }
-        else if (!(bool)have_at) // c
-        {
-            if (part[i] == '@' && i > 0)
-            {
-                return true;
-            }
-            else if (part[i] == '@')
+            if (part[i] == '.' && i < 1 ? true : part[i - 1] == '.') // checking for the first '.' and for two '.' in a row.
             {
                 return false;
             }
-            else if (true)
+            else if (part[i] >= 'a' && part[i] <= 'z' || part[i] >= 'A' && part[i] <= 'Z') // checking for a character from the alphabet.
             {
-                string correctedSymbol = "!#$%&'*+-/=?^_`{|}~";
-                for (int j = 0; j < correctedSymbol.length(); j++)
+                // break;
+            }
+            else if (!(bool)have_at) //
+            {
+                if (part[i] == '@' && i > 0 && part[i - 1] == '.')
                 {
-                    if (part[i] == correctedSymbol[j])
+                    return i;
+                }
+                else if (part[i] == '@')
+                {
+                    return false;
+                }
+                else if (true)
+                {
+                    string correctedSymbol = "!#$%&'*+-/=?^_`{|}~";
+                    for (int j = 0; j < correctedSymbol.length(); j++)
                     {
-                        break;
+                        if (part[i] == correctedSymbol[j])
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            return false;
+                        }
                     }
-                    else
-                    {
-                        return false;
-                    }
+                }
+                else
+                {
+                    return false;
                 }
             }
             else
@@ -46,6 +55,21 @@ int corrected_part(int have_at, string part)
                 return false;
             }
         }
-        return false;
     }
+}
+int main()
+{
+    int integerIndex(0);
+    string e_mail;
+    cout << "Enter E-mail: ";
+    cin >> e_mail;
+    if (corrected_part(integerIndex, e_mail))
+    {
+        integerIndex = corrected_part(integerIndex, e_mail);
+        if (corrected_part(integerIndex, e_mail))
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+
 }
