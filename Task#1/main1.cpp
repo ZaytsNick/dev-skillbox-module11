0f8c7bb962b6372753ad9be1c1ee4e124ec96843
Zayts Work <zaytsev@un74.ru>
2023-08-15 11:25:40 +0500

    task#1 accepted

--- Task#1/main1.cpp
+++ Task#1/main1.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
 using namespace std;
+char encrypt_symbol(char symbol, int shiftS, char alphabet)
+{
+  return (symbol % alphabet + shiftS) % 26 + (shiftS > 0 ? alphabet : alphabet + 26);
+}
+
 string encrypt_caesar(string textEC, int shiftEC)
 {
-  //task
-  for(int i=0; i<textEC.length(); i++)
+  // task
+
+  for (int i = 0; i < textEC.length(); i++)
   {
-    if      (textEC[i]>='a' && textEC[i]<='z') {textEC[i]+=(textEC[i]%'a'+shiftEC)%26+'a';}
-    else if (textEC[i]>='A' && textEC[i]<='Z') {textEC[i]+=(textEC[i]%'A'+shiftEC)%26+'A';}
-    else if (textEC[i]>='0' && textEC[i]<='9') {textEC[i]+=(textEC[i]%'0'+shiftEC)%26+'0';}
-    else 
+    if (textEC[i] >= 'a' && textEC[i] <= 'z')
+      textEC[i] = encrypt_symbol(textEC[i], shiftEC, 'a');
+    else if (textEC[i] >= 'A' && textEC[i] <= 'Z')
+      textEC[i] = encrypt_symbol(textEC[i], shiftEC, 'A');
+    else
     {
-      cout<<"Not correct symbol"<<endl;
-      return; 
+      cout << "Not correct symbol" << endl;
+      return "Not correct symbol";
     }
   }
   return textEC;
 }
 string decrypt_caesar(string textDC, int shiftDC)
 {
-  return encrypt_caesar(textDC,-shiftDC);
+  return encrypt_caesar(textDC, -shiftDC);
 }
-void main()
+int main()
 {
-  string text,text_encrypt_caesar, text_decrypt_caesar;
+  string text, text_encrypt_caesar, text_decrypt_caesar;
   int shift;
-  cout<<"Enter text: "<<endl;
-  cin>>text;
-  cout<<"Enter shift: ";
-  cin>>shift;
-  text_encrypt_caesar=encrypt_caesar(text,shift);
-  text_decrypt_caesar=decrypt_caesar(text_encrypt_caesar,shift);
+  cout << "Enter text: ";
+  cin >> text;
+  cout << "Enter shift: ";
+  cin >> shift;
+  text_encrypt_caesar = encrypt_caesar(text, shift);
+  text_decrypt_caesar = decrypt_caesar(text_encrypt_caesar, shift);
+  cout << "Text encrypt:\n"
+       << text_encrypt_caesar << "\nText decrypt:\n"
+       << text_decrypt_caesar << endl;
 }
