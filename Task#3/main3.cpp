daf3b563a290b7eb99f2ab99cb43ca6fb4bc8566
Zayts Work <zaytsev@un74.ru>
2023-08-17 12:15:13 +0500

    3

--- Task#3/main3.cpp
+++ Task#3/main3.cpp
@@ -5,13 +5,15 @@ using namespace std;
 bool Valid_part(string value)
 {
     int intValue(0);
-    for (int j = value.length() - 1; j >= 0; j--)
+    for (int j = value.length() - 2; j >= 0; j--)
     {
-        int a = (10 * (value.length() - 1 - j));
+        int a = (10 * (value.length() - 2 - j));
 
-        intValue += (value[j] - '0') * pow(10, (value.length() - 1 - j));
+        intValue += (value[j] - '0') * pow(10, (value.length() - 2 - j));
     }
-    if (intValue >= 0 && intValue <= 255)
+    if (intValue > 0 && intValue <= 255 && value[0] != '0')
+        return true;
+    else if (intValue == 0 && value == "0." || value == "0")
         return true;
     else
         return false;
@@ -25,18 +27,17 @@ string Valid_ip(string ip)
     int dot(0);
     for (int i = 0; i < ip.length(); i++)
     {
-        if (ip[i] == '.' && ip[i - 1] == '.')
+        if (ip[i] == '.' && ip[i - 1] == '.' || ip[i] < '0' && ip[i]>'9')
         {
             return "Invalid";
         }
         else if (ip[i] == '.' || (i == ip.length() - 1))
         {
             string part;
-            for (dot; dot < i; dot++)
+            for (dot; dot <= i; dot++)
             {
                 part += ip[dot];
             }
-            dot++;
             if (!Valid_part(part))
                 return "Invalid";
         }
